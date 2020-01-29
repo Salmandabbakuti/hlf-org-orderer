21bbb15a98e9fe261af8d247183b9b7cc6429f59
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-01-29 18:29:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,13 +14,11 @@ sleep 2
 echo 'org1 peers joining channel'
 sudo docker exec -it cli peer channel join -b mychannel.block
 sudo docker exec -e CORE_PEER_ADDRESS=peer1.org1.example.com:7051 -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt cli peer channel join -b mychannel.block
-sudo docker exec -it cli peer channel join -b mychannel.block
 echo 'exporting mychannel.block to cli2 container and Org2 peers will be joining..'
 sudo docker cp cli:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block .
 sudo docker cp mychannel.block cli2:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block
 sudo docker exec -it cli2 peer channel join -b mychannel.block
 docker exec -e CORE_PEER_ADDRESS=peer1.org2.example.com:7051 -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/ca.crt cli peer channel join -b mychannel.block
-sudo docker exec -it cli2 peer channel join -b mychannel.block
 sudo rm -rf mychannel.block
 echo 'All Peers joined Channel and Ready to test.'
 
